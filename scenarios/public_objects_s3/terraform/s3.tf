8b96e138df860e03d232ff8129f8f66f61e27d0d
Emanuel Valente <emanuelvalente@gmail.com>
2021-06-29 22:15:37 -0300

    update scenario

--- scenarios/public_objects_s3/terraform/s3.tf
+++ scenarios/public_objects_s3/terraform/s3.tf
@@ -1,52 +1,44 @@
 #Secret S3 Bucket
-resource "aws_s3_bucket" "cg-cardholder-data-bucket" {
-  bucket = "cg-cardholder-data-bucket-${var.cgid}"
+resource "aws_s3_bucket" "cg-public-objects-data-bucket" {
+  bucket = "cg-public-objects-data-bucket-${var.cgid}"
   acl = "private"
   force_destroy = true
+
   tags = {
-      Name = "cg-cardholder-data-bucket-${var.cgid}"
-      Description = "CloudGoat ${var.cgid} S3 Bucket used for storing sensitive cardholder data."
-      Stack = "${var.stack-name}"
-      Scenario = "${var.scenario-name}"
+      Name = "cg-public-objects-data-bucket-${var.cgid}"
+      Description = "CloudGoat ${var.cgid} S3 Bucket used for storing sensitive data."
+      Stack = var.stack-name
+      Scenario = var.scenario-name
   }
 }
-resource "aws_s3_bucket_object" "cardholder-data-primary" {
-  bucket = "${aws_s3_bucket.cg-cardholder-data-bucket.id}"
-  key = "cardholder_data_primary.csv"
-  source = "../assets/cardholder_data_primary.csv"
-  tags = {
-    Name = "cardholder-data-${var.cgid}"
-    Stack = "${var.stack-name}"
-    Scenario = "${var.scenario-name}"
+
+resource "aws_s3_bucket_public_access_block" "block_bucket" {
+  bucket = aws_s3_bucket.cg-public-objects-data-bucket.id
+
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false
+}
+
+
+resource "aws_s3_bucket_object" "objects" {
+for_each = fileset("../assets/", "*")
+bucket = aws_s3_bucket.cg-public-objects-data-bucket.id
+key = each.value
+source = "../assets/${each.value}"
+etag = filemd5("../assets/${each.value}")
+tags = {
+    Stack = var.stack-name
+    Scenario = var.scenario-name
   }
 }
-resource "aws_s3_bucket_object" "cardholder-data-secondary" {
-  bucket = "${aws_s3_bucket.cg-cardholder-data-bucket.id}"
-  key = "cardholder_data_secondary.csv"
-  source = "../assets/cardholder_data_secondary.csv"
-  tags = {
-    Name = "cardholder-data-${var.cgid}"
-    Stack = "${var.stack-name}"
-    Scenario = "${var.scenario-name}"
-  }
-}
-resource "aws_s3_bucket_object" "cardholder-data-corporate" {
-  bucket = "${aws_s3_bucket.cg-cardholder-data-bucket.id}"
-  key = "cardholders_corporate.csv"
-  source = "../assets/cardholders_corporate.csv"
-  tags = {
-    Name = "cardholder-data-${var.cgid}"
-    Stack = "${var.stack-name}"
-    Scenario = "${var.scenario-name}"
-  }
-}
-resource "aws_s3_bucket_object" "goat" {
-  bucket = "${aws_s3_bucket.cg-cardholder-data-bucket.id}"
-  key = "goat.png"
-  source = "../assets/goat.png"
-  tags = {
-    Name = "cardholder-data-${var.cgid}"
-    Stack = "${var.stack-name}"
-    Scenario = "${var.scenario-name}"
-  }
+
+# Upload our flag
+resource "aws_s3_bucket_object" "object" {
+  bucket = aws_s3_bucket.cg-public-objects-data-bucket.id
+  key    = "flag.txt"
+  acl    = "public-read" 
+  source = "../flag/flag.txt"
+  etag = filemd5("../flag/flag.txt")
 }
